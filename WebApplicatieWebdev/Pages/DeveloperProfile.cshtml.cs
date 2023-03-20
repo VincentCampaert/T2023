bbfb1d210b311a4834ce73080b4e5c970e6d8618
Niels <nielsvwarmerdam@gmail.com>
2023-03-20 10:05:42 +0100

    Initial

---
+++ WebApplicatieWebdev/Pages/DeveloperProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicatieWebdev.Pages
+{
+    public class DeveloperProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebCommon/Middleware/RequestCountMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebCommon.Middleware
+{
+    public class RequestCountMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestCountMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            int count = 1;
+            if (context.Request.Cookies.ContainsKey("requestCount"))
+            {
+                count = int.Parse(context.Request.Cookies["requestCount"]) + 1;
+            }
+            context.Response.Cookies.Append("requestCount", count.ToString());
+            await _next(context);
+        }
+    }
+}
